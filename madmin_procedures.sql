dd360af41f0208bf8e94c559b3c8cee19cbbe3db
pruthvi1405 <113937653+pruthvi1405@users.noreply.github.com>
2023-04-10 19:27:57 -0400

    Add files via upload
    scheduled_shows and theatre procedures added

--- madmin_procedures.sql
+++ madmin_procedures.sql
@@ -1,6 +1,6 @@
 select user from dual;
 
-
+---movie table procedures----
 CREATE OR REPLACE PROCEDURE add_movie (
     p_movie_name IN VARCHAR2,
     p_movie_description IN VARCHAR2,
@@ -108,6 +108,153 @@ END;
 
 
 select * from movie;
+
+--------theatre table updates-----
+
+CREATE OR REPLACE PROCEDURE create_theatre (
+    theatre_name IN VARCHAR2,
+    location_id IN NUMBER
+)
+IS
+BEGIN
+    INSERT INTO theatre (theatre_name, location_id)
+    VALUES (theatre_name, location_id);
+    COMMIT;
+    DBMS_OUTPUT.PUT_LINE('Theatre created successfully.');
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('Error while creating theatre: ' || SQLERRM);
+        ROLLBACK;
+END;
+/
+
+-- Procedure to update a theatre record
+CREATE OR REPLACE PROCEDURE update_theatre (
+    p_theatre_id IN theatre.theatre_id%TYPE,
+    p_theatre_name IN theatre.theatre_name%TYPE,
+    p_location_id IN theatre.location_id%TYPE
+) IS
+BEGIN
+    UPDATE theatre
+    SET theatre_name = p_theatre_name,
+        location_id = p_location_id
+    WHERE theatre_id = p_theatre_id;
+    
+    COMMIT;
+END;
+/
+
+
+-- Procedure to delete a theatre record
+CREATE OR REPLACE PROCEDURE delete_theatre (
+    p_theatre_id IN theatre.theatre_id%TYPE
+) IS
+BEGIN
+    DELETE FROM theatre
+    WHERE theatre_id = p_theatre_id;
+    
+    COMMIT;
+END;
+/
+
+
+select * from theatre;
+select * from theatre_location;
+
+BEGIN
+    create_theatre('Orion Mall', 1);
+END;
+/
+
+BEGIN
+    update_theatre(17, 'Orion Mall', 2);
+END;
+/
+
+BEGIN
+    delete_theatre(17);
+END;
+/
+
+------scheduled show---------
+
+CREATE OR REPLACE PROCEDURE create_scheduled_show (
+    p_screen_id IN scheduled_show.screen_id%TYPE,
+    p_movie_id IN scheduled_show.movie_id%TYPE,
+    p_start_date_time IN scheduled_show.start_date_time%TYPE,
+    p_end_date_time IN scheduled_show.end_date_time%TYPE
+) AS
+BEGIN
+    INSERT INTO scheduled_show (screen_id, movie_id, start_date_time, end_date_time)
+    VALUES (p_screen_id, p_movie_id, p_start_date_time, p_end_date_time);
+    COMMIT;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE get_scheduled_show (
+    p_show_id IN scheduled_show.show_id%TYPE,
+    p_screen_id OUT scheduled_show.screen_id%TYPE,
+    p_movie_id OUT scheduled_show.movie_id%TYPE,
+    p_start_date_time OUT scheduled_show.start_date_time%TYPE,
+    p_end_date_time OUT scheduled_show.end_date_time%TYPE
+) AS
+BEGIN
+    SELECT screen_id, movie_id, start_date_time, end_date_time
+    INTO p_screen_id, p_movie_id, p_start_date_time, p_end_date_time
+    FROM scheduled_show
+    WHERE show_id = p_show_id;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE update_scheduled_show (
+    p_show_id IN scheduled_show.show_id%TYPE,
+    p_screen_id IN scheduled_show.screen_id%TYPE,
+    p_movie_id IN scheduled_show.movie_id%TYPE,
+    p_start_date_time IN scheduled_show.start_date_time%TYPE,
+    p_end_date_time IN scheduled_show.end_date_time%TYPE
+) AS
+BEGIN
+    UPDATE scheduled_show
+    SET screen_id = p_screen_id,
+        movie_id = p_movie_id,
+        start_date_time = p_start_date_time,
+        end_date_time = p_end_date_time
+    WHERE show_id = p_show_id;
+    COMMIT;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE delete_scheduled_show (
+    p_show_id IN scheduled_show.show_id%TYPE
+)
+IS
+BEGIN
+    DELETE FROM ticket
+    WHERE show_id = p_show_id;
+
+    DELETE FROM scheduled_show
+    WHERE show_id = p_show_id;
+END;
+/
+
+
+select * from scheduled_show;
+
+BEGIN
+    create_scheduled_show(1, 2, '2023-05-01 18:30:00', '2023-05-01 21:00:00');
+END;
+
+BEGIN
+    update_scheduled_show(1, 2, 3, '2023-05-01 19:00:00', '2023-05-01 22:00:00');
+END;
+
+BEGIN
+    delete_scheduled_show(1);
+END;
+
+------seats-----
+
+select * from seat;
 
 /*
 CREATE TYPE movie_obj AS OBJECT (
